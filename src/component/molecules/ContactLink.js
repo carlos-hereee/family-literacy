fedd7e88105241119befb7276a479882b4f2629d
ch04937 <97hernandez.c@gmail.com>
2022-12-16 20:37:40 -0800

    adding app  content

--- src/component/molecules/ContactLink.js
+++ src/component/molecules/ContactLink.js
@@ -6,10 +6,19 @@ const ContactLink = ({ data }) => {
       <Icons name={data.icon} size="2x" />
       <div>
         <h3 className="title">{data.title}</h3>
-        {data.icon === "email" && (
-          <a href={`mailto:${data.description}`}> {data.description} </a>
-        )}
-        <p>{data.description}</p>
+        <address>
+          {data.icon === "email" ? (
+            <a href={`mailto:${data.description}`}>{data.description} </a>
+          ) : data.icon === "phone" ? (
+            <a href={`tel:${data.description}`}>{data.description} </a>
+          ) : data.icon === "business" ? (
+            <a href={`https://maps.google.com/?q=${data.address}`}>
+              {data.description}
+            </a>
+          ) : (
+            <p>{data.description}</p>
+          )}
+        </address>
       </div>
     </div>
   );
